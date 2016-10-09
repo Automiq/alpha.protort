b10c2b33def5c37cc3dd8652bf9ce42f80f183b8
Sergey M․ <dstftw@gmail.com>
2016-10-10 02:02:21 +0700

    [protocol/tests/auto] Cleanup

--- protocol/tests/auto/tests_packet.cpp
+++ protocol/tests/auto/tests_packet.cpp
@@ -1,40 +1,59 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/test/unit_test_suite.hpp>
+#include <boost/scope_exit.hpp>
 
 #include "packet.pb.h"
-
-using namespace alpha::protort::protocol;
 
 namespace alpha {
 namespace protort {
 namespace protocol {
 namespace tests {
 
-BOOST_AUTO_TEST_SUITE(tests_packet)
+using namespace alpha::protort::protocol;
+
+struct fixture
+{
+    ~fixture()
+    {
+        // Данный код будет вызван по окончании каждого теста, определенного
+        // в рамках одного test suite.
+        google::protobuf::ShutdownProtobufLibrary();
+    }
+};
+
+BOOST_FIXTURE_TEST_SUITE(tests_packet, fixture)
 
 BOOST_AUTO_TEST_CASE(test_serialize_parse)
 {
+    // Исходные данные
     const uint32_t id = 1;
-    const Packet::ComponentKind component = Packet::Generator;
-    const std::string str = "afwrger";
-    Packet newpacket;
-    newpacket.set_id(id);
-    BOOST_CHECK_EQUAL(newpacket.id(), id);
-    newpacket.set_component_kind(component);
-    BOOST_CHECK_EQUAL(newpacket.component_kind(), component);
+    const Packet::ComponentKind componentKind = Packet::Generator;
+    const std::string payload = "payload";
 
-    newpacket.set_payload(str);
+    // Пакет
+    Packet packet;
 
-    BOOST_CHECK_EQUAL(newpacket.payload(), str);
+    // Формируем пакет
+    packet.set_id(id);
+    packet.set_component_kind(componentKind);
+    packet.set_payload(payload);
 
-    auto serializedPacketAsStr = newpacket.SerializeAsString();
+    // Проверяем корректность сформированного пакета
+    BOOST_CHECK_EQUAL(packet.id(), id);
+    BOOST_CHECK_EQUAL(packet.component_kind(), componentKind);
+    BOOST_CHECK_EQUAL(packet.payload(), payload);
 
+    // Сериализуем пакет в строку
+    auto serializedPacket = packet.SerializeAsString();
+
+    // Десериализуем пакет из строки
     Packet deserializedPacket;
-    deserializedPacket.ParseFromString(serializedPacketAsStr);
-    BOOST_CHECK_EQUAL(deserializedPacket.payload(), str);
-    BOOST_CHECK_EQUAL(deserializedPacket.component_kind(), component);
+    deserializedPacket.ParseFromString(serializedPacket);
+
+    // Проверяем корректность десериализованного пакета
     BOOST_CHECK_EQUAL(deserializedPacket.id(), id);
-    google::protobuf::ShutdownProtobufLibrary();
+    BOOST_CHECK_EQUAL(deserializedPacket.component_kind(), componentKind);
+    BOOST_CHECK_EQUAL(deserializedPacket.payload(), payload);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
