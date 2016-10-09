647bad4fa4dcd18d35634fa59636677b9890293c
ZHURAVLEVSS <discografiya@gmail.com>
2016-10-09 11:43:34 -0700

    Реализована базовая версия протокола
    Closes #2

--- protocol/tests/auto/tests_packet.cpp
+++ protocol/tests/auto/tests_packet.cpp
@@ -3,30 +3,38 @@
 
 #include "packet.pb.h"
 
+using namespace alpha::protort::protocol;
+
 namespace alpha {
 namespace protort {
 namespace protocol {
 namespace tests {
 
-namespace _app = alpha::protort::protocol;
-
 BOOST_AUTO_TEST_SUITE(tests_packet)
 
 BOOST_AUTO_TEST_CASE(test_serialize_parse)
 {
-    const uint32_t value = 42;
+    const uint32_t id = 1;
+    const Packet::ComponentKind component = Packet::Generator;
+    const std::string str = "afwrger";
+    Packet newpacket;
+    newpacket.set_id(id);
+    BOOST_CHECK_EQUAL(newpacket.id(), id);
+    newpacket.set_component_kind(component);
+    BOOST_CHECK_EQUAL(newpacket.component_kind(), component);
 
-    _app::Packet packet;
-    packet.set_foobar(value);
+    newpacket.set_payload(str);
 
-    BOOST_CHECK_EQUAL(packet.foobar(), value);
+    BOOST_CHECK_EQUAL(newpacket.payload(), str);
 
-    auto serializedAsString = packet.SerializeAsString();
+    auto serializedPacketAsStr = newpacket.SerializeAsString();
 
-    _app::Packet deserializedPacket;
-    deserializedPacket.ParseFromString(serializedAsString);
-
-    BOOST_CHECK_EQUAL(deserializedPacket.foobar(), value);
+    Packet deserializedPacket;
+    deserializedPacket.ParseFromString(serializedPacketAsStr);
+    BOOST_CHECK_EQUAL(deserializedPacket.payload(), str);
+    BOOST_CHECK_EQUAL(deserializedPacket.component_kind(), component);
+    BOOST_CHECK_EQUAL(deserializedPacket.id(), id);
+    google::protobuf::ShutdownProtobufLibrary();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
