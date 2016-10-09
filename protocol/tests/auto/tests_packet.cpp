ffd01242ecaecce93cf2df5d0577a2640b677454
ZHURAVLEVSS <discografiya@gmail.com>
2016-10-09 14:02:18 -0700

    Протокол сделан

--- protocol/tests/auto/tests_packet.cpp
+++ protocol/tests/auto/tests_packet.cpp
@@ -1,32 +1,59 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/test/unit_test_suite.hpp>
+#include <iostream>
 
 #include "packet.pb.h"
+
+namespace _app = alpha::protort::protocol;
+using namespace std;
+using namespace _app;
 
 namespace alpha {
 namespace protort {
 namespace protocol {
 namespace tests {
 
-namespace _app = alpha::protort::protocol;
-
 BOOST_AUTO_TEST_SUITE(tests_packet)
 
 BOOST_AUTO_TEST_CASE(test_serialize_parse)
 {
-    const uint32_t value = 42;
+    const uint32_t id = 1;
+    string str0 = "afegrgb";
+    string str1 = "aevetbfegrgb";
+    cout << "Welcome to test exe (Zhuravlev SS):" << endl;
+    Packet newpacket;
+    text newtext;
+    newpacket.set_id(id);
+    BOOST_CHECK_EQUAL(newpacket.id(), id);
+    newpacket.set_component(Packet::CalcSin);
+    BOOST_CHECK_EQUAL(newpacket.component(), Packet::CalcSin);
+    newtext.add_mes(str0.c_str());
+    newtext.add_mes(str1.c_str());
+    BOOST_CHECK_EQUAL(newtext.mes(0), str0);
+    BOOST_CHECK_EQUAL(newtext.mes(1), str1);
 
-    _app::Packet packet;
-    packet.set_foobar(value);
+    auto serializedTextAsStr = newtext.SerializeAsString();
 
-    BOOST_CHECK_EQUAL(packet.foobar(), value);
+    text deserializedtext;
+    deserializedtext.ParseFromString(serializedTextAsStr);
 
-    auto serializedAsString = packet.SerializeAsString();
+    BOOST_CHECK_EQUAL(deserializedtext.mes(0), str0);
+    BOOST_CHECK_EQUAL(deserializedtext.mes(1), str1);
 
-    _app::Packet deserializedPacket;
-    deserializedPacket.ParseFromString(serializedAsString);
+    newpacket.set_allocated_masbytes(&serializedTextAsStr);
 
-    BOOST_CHECK_EQUAL(deserializedPacket.foobar(), value);
+    auto serializedPacketAsStr = newpacket.SerializeAsString();
+
+    Packet deserializedPacket;
+    deserializedPacket.ParseFromString(serializedPacketAsStr);
+    BOOST_CHECK_EQUAL(deserializedPacket.masbytes(), serializedTextAsStr);
+    BOOST_CHECK_EQUAL(deserializedPacket.component(), Packet::CalcSin);
+    cout << deserializedPacket.id()<< endl;
+    cout << id << endl;
+    BOOST_CHECK_EQUAL(deserializedPacket.id(), id);
+
+    cout << "Everything is well done" << endl;
+    cin.get();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
