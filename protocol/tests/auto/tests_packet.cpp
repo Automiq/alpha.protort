a048db5f2fcc10bf377d7d0acce8c9a285d5febf
ZHURAVLEVSS <discografiya@gmail.com>
2016-10-09 18:36:00 -0700

    Исправлено

--- protocol/tests/auto/tests_packet.cpp
+++ protocol/tests/auto/tests_packet.cpp
@@ -4,9 +4,8 @@
 
 #include "packet.pb.h"
 
-namespace _app = alpha::protort::protocol;
 using namespace std;
-using namespace _app;
+using namespace alpha::protort::protocol;
 
 namespace alpha {
 namespace protort {
@@ -18,41 +17,25 @@ BOOST_AUTO_TEST_SUITE(tests_packet)
 BOOST_AUTO_TEST_CASE(test_serialize_parse)
 {
     const uint32_t id = 1;
-    string str0 = "afegrgb";
-    string str1 = "aevetbfegrgb";
-    cout << "Welcome to test exe (Zhuravlev SS):" << endl;
     Packet newpacket;
-    text newtext;
     newpacket.set_id(id);
     BOOST_CHECK_EQUAL(newpacket.id(), id);
-    newpacket.set_component(Packet::CalcSin);
-    BOOST_CHECK_EQUAL(newpacket.component(), Packet::CalcSin);
-    newtext.add_mes(str0.c_str());
-    newtext.add_mes(str1.c_str());
-    BOOST_CHECK_EQUAL(newtext.mes(0), str0);
-    BOOST_CHECK_EQUAL(newtext.mes(1), str1);
+    newpacket.set_component(Packet::Generator);
+    BOOST_CHECK_EQUAL(newpacket.component(), Packet::Generator);
 
-    auto serializedTextAsStr = newtext.SerializeAsString();
+    auto serializedPayloadAsStr = newpacket.SerializeAsString();
 
-    text deserializedtext;
-    deserializedtext.ParseFromString(serializedTextAsStr);
+    newpacket.set_allocated_payload(&serializedPayloadAsStr);
 
-    BOOST_CHECK_EQUAL(deserializedtext.mes(0), str0);
-    BOOST_CHECK_EQUAL(deserializedtext.mes(1), str1);
-
-    newpacket.set_allocated_masbytes(&serializedTextAsStr);
+    BOOST_CHECK_EQUAL(newpacket.payload(), serializedPayloadAsStr);
 
     auto serializedPacketAsStr = newpacket.SerializeAsString();
 
     Packet deserializedPacket;
     deserializedPacket.ParseFromString(serializedPacketAsStr);
-    BOOST_CHECK_EQUAL(deserializedPacket.masbytes(), serializedTextAsStr);
-    BOOST_CHECK_EQUAL(deserializedPacket.component(), Packet::CalcSin);
-    cout << deserializedPacket.id()<< endl;
-    cout << id << endl;
+    BOOST_CHECK_EQUAL(deserializedPacket.payload(), serializedPayloadAsStr);
+    BOOST_CHECK_EQUAL(deserializedPacket.component(), Packet::Generator);
     BOOST_CHECK_EQUAL(deserializedPacket.id(), id);
-
-    cout << "Everything is well done" << endl;
     cin.get();
 }
 
