4ca8055798943e9c3f55c84920e8eb97817649f5
Pavel Rabcheniuk <d.walker.aug@yandex.ru>
2016-10-11 11:57:43 +0700

    Исправления

--- boost.pri
+++ boost.pri
@@ -3,4 +3,5 @@ LIBS += -L$$(BOOST_LIB_PATH)
 
 linux {
     LIBS += -lboost_unit_test_framework
+    DEFINES *= BOOST_TEST_DYN_LINK
 }
--- terminal/tests/auto/main.cpp
+++ terminal/tests/auto/main.cpp
@@ -1,12 +1,6 @@
 #define BOOST_TEST_MODULE alpha.protort.terminal.tests
 
-//для компиляции под linux
-#ifdef __linux__
 
-#define BOOST_TEST_MAIN
-#define BOOST_TEST_DYN_LINK
-
-#endif
 
 #include <boost/test/unit_test.hpp>
 #include <boost/test/unit_test_suite.hpp>
