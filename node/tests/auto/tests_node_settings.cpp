f7a8049231683aa00463d7201c56ee8dba1004c1
Sergey M <dstftw@gmail.com>
2016-10-13 17:48:04 +0700

    Update tests_node_settings.cpp

--- node/tests/auto/tests_node_settings.cpp
+++ node/tests/auto/tests_node_settings.cpp
@@ -15,7 +15,6 @@ BOOST_AUTO_TEST_CASE(test_node_setiings_parse)
     alpha::protort::node::node_settings ns;
     const char *argv[] = { "--s", "192.168.1.1:999" };
     BOOST_CHECK(ns.parse(2, argv));
-
 }
 
 BOOST_AUTO_TEST_SUITE_END()
