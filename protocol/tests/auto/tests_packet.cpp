b48b2324625848e4c86eed12df9473f0a3d40055
Yaroslav Shmakov <123@12.ru>
2016-10-19 19:27:57 +0700

    Добавлены изменения в протокол: Сформирована сущность Endpoint

--- protocol/tests/auto/tests_packet.cpp
+++ protocol/tests/auto/tests_packet.cpp
@@ -2,6 +2,7 @@
 #include <boost/test/unit_test_suite.hpp>
 
 #include "packet.pb.h"
+#include "iostream"
 
 namespace alpha {
 namespace protort {
@@ -24,22 +25,32 @@ BOOST_FIXTURE_TEST_SUITE(tests_packet, fixture)
 
 BOOST_AUTO_TEST_CASE(test_serialize_parse)
 {
-    // Исходные данные
-    const uint32_t id = 1;
-    const Packet::ComponentKind componentKind = Packet::Generator;
-    const std::string payload = "payload";
+	// Исходные данные
+	const uint32_t id = 1;
+    const std::string payload = "hello_worm";
 
-    // Пакет
-    Packet packet;
+    ComponentEndpoint sourceComponent;
+    sourceComponent.set_component_kind(ComponentKind::Generator);
+    sourceComponent.set_port(2);
+	
+    ComponentEndpoint destComponent;
+    destComponent.set_component_kind(ComponentKind::Retranslator);
+    destComponent.set_port(2);
 
     // Формируем пакет
-    packet.set_id(id);
-    packet.set_component_kind(componentKind);
+    Packet packet;
+    packet.set_id(1);
     packet.set_payload(payload);
+
+    packet.mutable_source()->set_component_kind(sourceComponent);
+    packet.set_allocated_destination(&destComponent);
 
     // Проверяем корректность сформированного пакета
     BOOST_CHECK_EQUAL(packet.id(), id);
-    BOOST_CHECK_EQUAL(packet.component_kind(), componentKind);
+    BOOST_CHECK_EQUAL(packet.source().component_kind(), sourceComponent.component_kind());
+    BOOST_CHECK_EQUAL(packet.source().port(), sourceComponent.port());
+    BOOST_CHECK_EQUAL(packet.destination().component_kind(), destComponent.component_kind());
+    BOOST_CHECK_EQUAL(packet.destination().port(), destComponent.port());
     BOOST_CHECK_EQUAL(packet.payload(), payload);
 
     // Сериализуем пакет в строку
@@ -51,7 +62,11 @@ BOOST_AUTO_TEST_CASE(test_serialize_parse)
 
     // Проверяем корректность десериализованного пакета
     BOOST_CHECK_EQUAL(deserializedPacket.id(), id);
-    BOOST_CHECK_EQUAL(deserializedPacket.component_kind(), componentKind);
+    BOOST_CHECK_EQUAL(deserializedPacket.source().component_kind(), sourceComponent.component_kind());
+    BOOST_CHECK_EQUAL(deserializedPacket.source().port(), sourceComponent.port());
+    BOOST_CHECK_EQUAL(deserializedPacket.destination().component_kind(), destComponent.component_kind());
+    BOOST_CHECK_EQUAL(deserializedPacket.destination().port(), destComponent.port());
+    std::cout<<payload;
     BOOST_CHECK_EQUAL(deserializedPacket.payload(), payload);
 }
 
