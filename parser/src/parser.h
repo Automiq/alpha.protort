de7a1c5eaf5f2c5a95ad3aa8e6e5efb239471fac
kimolegan <kimolegan@gmail.com>
2016-10-21 15:45:22 +0700

    Добавлены комментарии для doxygen

--- parser/src/parser.h
+++ parser/src/parser.h
@@ -9,7 +9,11 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/foreach.hpp>
-
+/*!
+ * \brief Класс компонент
+ *
+ * Класс содержит имя и тип компонента
+ */
 struct component
 {
     std::string name;
@@ -19,37 +23,95 @@ bool operator <(component const& left, component const& right)
 {
     return left.name < right.name;
 }
-
+/*!
+ * \brief Класс сетевого узла
+ *
+ * Класс хранит имя, адрес и порт узла
+ */
 struct node
 {
     std::string id;
     std::string ip;
     unsigned short port;
 };
-
+/*!
+ * \brief Класс, описывающий схему приложения и развертывания
+ *
+ * Данный класс получает и хранит информацию путем парсинга xml файлов схемы приложения
+ * и схемы развертывания.
+ */
 class Deploy_scheme
 {
 public:
     typedef std::pair<std::string, short> comp_name_port;
     typedef std::pair<comp_name_port, comp_name_port> connection;
+    /*!
+     * \brief Парсит схему приложения
+     * \param путь к файлу схемы приложения
+     *
+     * Метод парсит схему приложения и хранит полученную информацию внутри класса в соответствующих атрибутах
+     * Содержит обработчик ошибок, которые обычно возникают на этапе чтения xml файла, выводит сообщения
+     * об ошибках в стандартный вывод.
+     */
     void parse_app(const std::string &filename);
+    /*!
+     * \brief Парсит схему развертывания
+     * \param путь к файлу схемы развертыания
+     *
+     * Метод парсит схему развертывания и хранит полученную информацию внутри класса в соответствующих атрибутах
+     * Содержит обработчик ошибок, которые обычно возникают на этапе чтения xml файла, выводит сообщения
+     * об ошибках в стандартный вывод.
+     */
     void parse_deploy(const std::string &filename);
+    /*!
+     * \brief Находит компонент по его имени
+     * \param Имя компонента
+     * \return Компонент
+     */
     component& get_component(std::string& id)
     {
         return id_to_component[id];
     }
+    /*!
+     * \brief Находит узел по его имени
+     * \param Имя узла
+     * \return Узел
+     */
     std::string& get_node(std::string comp_name)
     {
         return component_to_node[comp_name];
     }
+    /*!
+     * \brief Находит имя  принимающего компонента и номер входа по имени отправителя и номера выхода
+     * \param Пара <Имя компонента, номер выхода>
+     * \return Пара <Имя компонента, номер выхода>
+     *
+     * В отображении связей ищет имя и вход получателя по имени и выходу отправителя
+     */
     comp_name_port get_dest_and_port(comp_name_port source_name_port)
     {
         return connections[source_name_port];
     }
 private:
+    /*!
+     * \brief Отображение имени компонента на сам компонент
+     */
     std::map<std::string, component> id_to_component;
+    /*!
+     * \brief Отображение имени узла на сам узел
+     */
     std::map<std::string, node> id_to_node;
+    /*!
+     * \brief Отображение связей между портами отправителя и получателя
+     * Ассоциативный контейнер, где ключом является пара <Имя отправляющего компонента, номер выхода>,
+     * а значением является пара <Имя принимающего компонента, номер входа>
+     */
     std::map<comp_name_port, comp_name_port> connections;
+    /*!
+     * \brief Отображение компонета на узел
+     * Ассоциативный контейнер, где ключом является имя компонента,
+     * а значением является имя узла, на котором этот компонент будет работать
+     */
     std::map<std::string, std::string> component_to_node;
 };
 
