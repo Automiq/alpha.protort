864e4b05d460a58e997bf28d15806aa97540ed06
Pavel Rabcheniuk <d.walker.aug@yandex.ru>
2016-11-01 17:08:47 +0700

    Написаны тесты для протокола

--- protocol/tests/auto/tests_packet.cpp
+++ protocol/tests/auto/tests_packet.cpp
@@ -69,6 +69,121 @@ BOOST_AUTO_TEST_CASE(test_serialize_parse)
     BOOST_CHECK_EQUAL(deserializedPacket.payload(), payload);
 }
 
+BOOST_AUTO_TEST_CASE(test_serialize_trerminal_to_node_protocol){
+    //формируем тестовые данные
+    const std::string node_name = "node";
+    const std::string node_addr = "localhost";
+    const uint32_t node_port = 100;
+
+    const std::string terminator_name = "terminator";
+    const ComponentKind terminator_kind = ComponentKind::Terminator;
+    const uint32_t terminator_port_out = 1, terminator_port_in = 0;
+
+    const std::string generator_name = "generator";
+    const ComponentKind generator_kind = ComponentKind::Generator;
+    const uint32_t generator_port_out = 1, generator_port_in = 0;
+
+    //пакет с информацией о ноде
+    NodeInfo info;
+    info.set_node_name(node_name);
+    info.set_node_addr(node_addr);
+    info.set_node_port(node_port);
+
+    NodeInfoList info_list;
+    info_list.add_nodeinfo()->CopyFrom(info);
+
+    BOOST_CHECK_EQUAL(info_list.nodeinfo(0).node_name(),node_name);
+    BOOST_CHECK_EQUAL(info_list.nodeinfo(0).node_addr(),node_addr);
+    BOOST_CHECK_EQUAL(info_list.nodeinfo(0).node_port(),node_port);
+
+    Instance terminator, generator;
+    terminator.set_kind(terminator_kind);
+    terminator.set_name(terminator_name);
+    generator.set_kind(generator_kind);
+    generator.set_name(generator_name);
+
+    InstanceList inst_list;
+    inst_list.add_instance()->CopyFrom(terminator);
+    inst_list.add_instance()->CopyFrom(generator);
+
+    BOOST_CHECK_EQUAL(inst_list.instance(0).kind(),terminator_kind);
+    BOOST_CHECK_EQUAL(inst_list.instance(0).name(),terminator_name);
+    BOOST_CHECK_EQUAL(inst_list.instance(1).kind(),generator_kind);
+    BOOST_CHECK_EQUAL(inst_list.instance(1).name(),generator_name);
+
+    Map terminator_map, generator_map;
+    terminator_map.set_node_name(node_name);
+    terminator_map.set_instance_name(terminator_name);
+    generator_map.set_node_name(node_name);
+    generator_map.set_instance_name(generator_name);
+
+    MapList map_list;
+    map_list.add_map()->CopyFrom(terminator_map);
+    map_list.add_map()->CopyFrom(generator_map);
+
+    BOOST_CHECK_EQUAL(map_list.map(0).node_name(),node_name);
+    BOOST_CHECK_EQUAL(map_list.map(0).instance_name(),terminator_name);
+    BOOST_CHECK_EQUAL(map_list.map(1).node_name(),node_name);
+    BOOST_CHECK_EQUAL(map_list.map(1).instance_name(),generator_name);
+
+    ComponentEndpoint from_generator, to_terminator;
+    from_generator.set_name(generator_name);
+    from_generator.set_port(generator_port_out);
+    to_terminator.set_name(terminator_name);
+    to_terminator.set_port(terminator_port_in);
+
+    Connection con;
+    con.mutable_source()->CopyFrom(from_generator);
+    con.mutable_destination()->CopyFrom(to_terminator);
+
+    ConnectionList con_list;
+    con_list.add_connection()->CopyFrom(con);
+
+    BOOST_CHECK_EQUAL(con_list.connection(0).source().name(),generator_name);
+    BOOST_CHECK_EQUAL(con_list.connection(0).source().port(),generator_port_out);
+    BOOST_CHECK_EQUAL(con_list.connection(0).destination().name(),terminator_name);
+    BOOST_CHECK_EQUAL(con_list.connection(0).destination().port(),terminator_port_in);
+    BOOST_CHECK_EQUAL(con_list.connection(0).remote_node_name(),"");
+
+    auto serialized_info_list = info_list.SerializeAsString(),
+         serialized_map_list  = map_list.SerializeAsString(),
+         serialized_inst_list = inst_list.SerializeAsString(),
+         serialized_con_list  = con_list.SerializeAsString();
+
+    NodeInfoList deserialized_info_list;
+    deserialized_info_list.ParseFromString(serialized_info_list);
+
+    MapList deserialized_map_list;
+    deserialized_map_list.ParseFromString(serialized_map_list);
+
+    InstanceList deserialized_inst_list;
+    deserialized_inst_list.ParseFromString(serialized_inst_list);
+
+    ConnectionList deserialized_con_list;
+    deserialized_con_list.ParseFromString(serialized_con_list);
+
+    BOOST_CHECK_EQUAL(deserialized_info_list.nodeinfo(0).node_name(),node_name);
+    BOOST_CHECK_EQUAL(deserialized_info_list.nodeinfo(0).node_addr(),node_addr);
+    BOOST_CHECK_EQUAL(deserialized_info_list.nodeinfo(0).node_port(),node_port);
+
+    BOOST_CHECK_EQUAL(deserialized_inst_list.instance(0).kind(),terminator_kind);
+    BOOST_CHECK_EQUAL(deserialized_inst_list.instance(0).name(),terminator_name);
+    BOOST_CHECK_EQUAL(deserialized_inst_list.instance(1).kind(),generator_kind);
+    BOOST_CHECK_EQUAL(deserialized_inst_list.instance(1).name(),generator_name);
+
+    BOOST_CHECK_EQUAL(deserialized_map_list.map(0).node_name(),node_name);
+    BOOST_CHECK_EQUAL(deserialized_map_list.map(0).instance_name(),terminator_name);
+    BOOST_CHECK_EQUAL(deserialized_map_list.map(1).node_name(),node_name);
+    BOOST_CHECK_EQUAL(deserialized_map_list.map(1).instance_name(),generator_name);
+
+    BOOST_CHECK_EQUAL(deserialized_con_list.connection(0).source().name(),generator_name);
+    BOOST_CHECK_EQUAL(deserialized_con_list.connection(0).source().port(),generator_port_out);
+    BOOST_CHECK_EQUAL(deserialized_con_list.connection(0).destination().name(),terminator_name);
+    BOOST_CHECK_EQUAL(deserialized_con_list.connection(0).destination().port(),terminator_port_in);
+    BOOST_CHECK_EQUAL(deserialized_con_list.connection(0).remote_node_name(),"");
+
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 } // namespace tests
