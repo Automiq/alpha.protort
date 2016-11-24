8d9e6ef825a86c96cc448a1c7ce67f8fb5489117
Pavel Rabcheniuk <d.walker.aug@yandex.ru>
2016-11-24 13:19:37 +0700

    Переделан генератор под схему демо-дня

--- components/src/generator.h
+++ components/src/generator.h
@@ -5,6 +5,9 @@
 #include <boost/thread.hpp>
 #include <boost/asio.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <cstring>
+#include <cstdlib>
+#include <sstream>
 
 #include "router.h"
 
@@ -20,13 +23,17 @@ public:
         generating_interval(3000),
         timer(router.get_service())
     {
-
+        std::srand(std::time(NULL));
     }
 
     output_list process(port_id input_port, std::string const & payload) final override
     {
-        std::string rand_str("Random_string");
-        return { { rand_str, { 0, 1 } } };
+        //std::string rand_str("Random_string");
+
+        std::ostringstream o;
+        o << rand(100,0,3) << ' ' << std::time(NULL);
+
+        return { { o.str(), { 0, 1 } } };
     }
     port_id in_port_count() const final override { return 0; }
     port_id out_port_count() const final override { return 2; }
@@ -37,9 +44,12 @@ public:
             return;
 
         // TODO generate meaningful data
-        std::string data("Generated data");
+        std::ostringstream o;
+        o << rand(100,0,3) << ' ' << std::time(NULL);
+        //std::string res(std::to_string(rand(100,0,3)) + std::to_string(std::time(NULL)));
 
-        router_.do_route(comp_inst_, { { data, { 0, 1 } } });
+
+        router_.do_route(comp_inst_,{ {o.str() , {0 , 1}} });
 
         timer.expires_from_now(boost::posix_time::milliseconds(generating_interval));
         timer.async_wait(boost::bind(&generator::generate, this));
@@ -56,6 +66,13 @@ public:
         started = false;
     }
 private:
+
+    float rand(int max,int min,int chars_after_point)
+    {
+        int div = std::pow(10,chars_after_point);
+        return float((std::rand() + min * div) % (max * div)) / div;
+    }
+
     boost::asio::deadline_timer timer;
     int generating_interval;
     bool started;
