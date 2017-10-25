85eb0c2aadc1cc1bd386ad08e9a18484f48d1526
Aleksandr Kuznetsov <alex_monster15@mail.ru>
2017-10-25 16:30:04 +0700

    Изменен компонент Parser. Было добавлено считывание поддерева pairnode(резервная нода) у node. Так же были добавлены дополнительные проверки: проверка на то, что резервный узел один и проверка на коректность задания узла.

--- parser/src/parser.h
+++ parser/src/parser.h
@@ -12,6 +12,16 @@ namespace alpha {
 namespace protort {
 namespace parser {
 
+/*!
+* \brief Компонент parser - описывает схему приложения и развертывания,
+* получает и хрании информацию путем парсинга xml файлов схемы приложения
+* и схемы развертывания.
+*
+* Стуктуры:
+* Компонента, связь между портами, сетевой узел, резервный узел
+* отображение компонента на сетевой узел, конфигурация.
+*/
+
 using port_id = uint32_t;
 
 /*!
@@ -19,6 +29,7 @@ using port_id = uint32_t;
  *
  * Класс содержит имя и тип компонента
  */
+
 struct component
 {
     std::string name;
@@ -30,25 +41,40 @@ struct component
  *
  * Класс хранит имена компонентов и связанные порты
  */
+
 struct connection
 {
-    std::string source;
-    port_id source_out;
-    std::string dest;
-    port_id dest_in;
+    std::string source;// Идентификатор компонента, отправляющего сообщение
+    port_id source_out;// Идентификатор выхода у отправляющего компонента
+    std::string dest;// Идентификатор компонента, получающего сообщение
+    port_id dest_in;// Идентификатор входа у получающего компонента
 };
 
 /*!
  * \brief Класс сетевого узла
  *
- * Класс хранит имя, адрес и порт узла
+ * Класс хранит имя, адрес, порт узла и конфигурационный порт узла
  */
+
 struct node
 {
-    std::string name;
-    std::string address;
-    port_id port;
-    port_id config_port;
+    std::string name;// Имя
+    std::string address;// IP-адрес или hostname узла
+    port_id port;// Порт
+    port_id config_port;// Порт сервера конфигурации (по умолчанию =100)
+};
+
+/*!
+ * \brief Класс резервного узла
+ *
+ * Класс адрес, порт узла и конфигурационный порт узла
+ */
+
+struct pairnode
+{
+    std::string address;// IP-адрес или hostname узла
+    port_id port;// Порт
+    port_id config_port;// Порт сервера конфигурации (по умолчанию =100)
 };
 
 /*!
@@ -56,6 +82,7 @@ struct node
  *
  * Класс хранит имя компонента и имя узла
  */
+
 struct mapping
 {
     std::string comp_name;
@@ -68,100 +95,173 @@ struct mapping
  * Данный класс получает и хранит информацию путем парсинга xml файлов схемы приложения
  * и схемы развертывания.
  */
-struct configuration
+
+struct configuration:std::exception
 {
+    //Векторы для хранения соответствующих компонентов после парсинга приложения
     std::vector<component> components;
     std::vector<connection> connections;
     std::vector<node> nodes;
+    std::vector<pairnode> pairnodes;
     std::vector<mapping> mappings;
 
     /*!
      * \brief Парсит схему приложения
-     * \param путь к файлу схемы приложения
+     * \param путь к файлу, в котором находится схема приложения в формате xml
      * \return true если успешно, иначе false
      *
      * Метод парсит схему приложения и хранит полученную информацию внутри класса в соответствующих атрибутах
      * Содержит обработчик ошибок, которые обычно возникают на этапе чтения xml файла, выводит сообщения
      * об ошибках в стандартный вывод.
      */
-    bool parse_app(const std::string &filename)
-    {
-        try
-        {
-            using boost::property_tree::ptree;
-            boost::property_tree::ptree pt;
-            read_xml(filename, pt);
 
-            BOOST_FOREACH( ptree::value_type const& v, pt.get_child("app") ) {
-                if( v.first == "instance" ) {
-                    component comp;
-                    comp.name = v.second.get<std::string>("<xmlattr>.name");
-                    comp.kind = v.second.get<std::string>("<xmlattr>.kind");
-                    components.push_back(comp);
-                }
-                else if( v.first == "connection" ) {
-                    connection conn;
-                    conn.source = v.second.get<std::string>("<xmlattr>.source");
-                    conn.source_out = v.second.get<port_id>("<xmlattr>.source_out");
-                    conn.dest = v.second.get<std::string>("<xmlattr>.dest");
-                    conn.dest_in = v.second.get<port_id>("<xmlattr>.dest_in");
-                    connections.push_back(conn);
-                }
-                else
-                    std::cout << "Unknown tag in the file" << std::endl;
+     bool parse_app(const std::string &filename)
+     {
+         try
+         {
+            boost::property_tree::ptree pt;// Cоздаем дерево
+            read_xml(filename, pt);// Парсим xml в дерево pt
+
+            BOOST_FOREACH( boost::property_tree::ptree::value_type const &v, pt.get_child("app") ) {// Создаем компоненты для тэгов app .
+            if( v.first == "instance" ) {
+                parse_component(v);
+            }
+            else if( v.first == "connection" ) {
+                parse_connection(v);
+            }
+            else
+                std::cout << "Unknown tag in the file" << std::endl;
             }
             return true;
-        }
-        catch(std::exception &e)
-        {
-            std::cout << "Error: " << e.what() << std::endl;
-            return false;
-        }
-    }
+         }
 
-    /*!
-     * \brief Парсит схему развертывания
-     * \param путь к файлу схемы развертыания
-     * \return true если успешно, иначе false
-     *
-     * Метод парсит схему развертывания и хранит полученную информацию внутри класса в соответствующих атрибутах
-     * Содержит обработчик ошибок, которые обычно возникают на этапе чтения xml файла, выводит сообщения
-     * об ошибках в стандартный вывод.
-     */
-    bool parse_deploy(const std::string &filename)
-    {
-        try
-        {
-            using boost::property_tree::ptree;
-            boost::property_tree::ptree pt;
-            read_xml(filename, pt);
+         catch(std::exception &error)
+         {
+             std::cout << "Error: " << error.what() << std::endl;
+             return false;
+         }
+     }
 
-            BOOST_FOREACH( ptree::value_type const& v, pt.get_child("deploy") ) {
-                if( v.first == "node" ) {
-                    node n;
-                    n.name = v.second.get<std::string>("<xmlattr>.name");
-                    n.address = v.second.get<std::string>("<xmlattr>.address");
-                    n.port = v.second.get<port_id>("<xmlattr>.port");
-                    n.config_port = v.second.get<port_id>("<xmlattr>.config_port", 100);
-                    nodes.push_back(n);
-                }
-                else if( v.first == "map" ) {
-                    mapping mapp;
-                    mapp.comp_name = v.second.get<std::string>("<xmlattr>.instance");
-                    mapp.node_name = v.second.get<std::string>("<xmlattr>.node");
-                    mappings.push_back(mapp);
-                }
-                else
-                    std::cout << "Unknown tag in the file" << std::endl;
-            }
-            return true;
-        }
-        catch(std::exception &e)
-        {
-            std::cout << "Error: " << e.what() << std::endl;
-            return false;
-        }
-    }
+
+     bool parse_deploy(const std::string &filename)
+     {
+         try
+         {
+             boost::property_tree::ptree pt;// Cоздаем дерево
+             read_xml(filename, pt);// Парсим xml в дерево pt
+
+             BOOST_FOREACH( boost::property_tree::ptree::value_type const &v, pt.get_child("deploy") ) {// Создаем компоненты для тегов deploy
+                 if( v.first == "node") {
+
+                     auto size = v.second.size();
+
+                     if(size > 2)
+                     {
+                         throw std::invalid_argument("The node name("
+                                                     + v.second.get<std::string>("<xmlattr>.name")
+                                                     + ") has more than one pairnode");
+                     }
+                     else{
+                         if(size == 0)
+                             throw std::exception("Incorrectly entered node!");
+                         else{
+                             parse_node(v);
+
+                             if(size == 2)
+                                parse_pairnode(v);
+                         }
+                     }
+                 }
+                 else if( v.first == "map" )
+                     parse_map(v);
+                 else
+                     std::cout << "Unknown tag in the file" << std::endl;
+             }
+             return true;
+         }
+
+         catch(std::exception &error)
+         {
+             std::cout << "Error: " << error.what() << std::endl;
+             return false;
+         }
+     }
+
+private:
+
+     /*!
+      * \brief Парсит node
+      *
+     * Метод парсит node и сохраняет полученную информацию внутри класса в соответствующих атрибутах
+      */
+
+     void parse_node(boost::property_tree::ptree::value_type const &v)
+     {
+         node noda;
+         noda.address = v.second.get<std::string>("<xmlattr>.address");// Устанавливаем  адресс узла, соответстующий иадресу из xml кода
+         noda.port = v.second.get<port_id>("<xmlattr>.port");// Устанавливаем  порт узла, соответстующий порту из xml кода
+         noda.config_port = v.second.get<port_id>("<xmlattr>.config_port", 100);//Устанавливаем  конфигурацию, соответстующую конфигурации из xml кода
+         nodes.push_back(noda);// Пушим узел в вектор узлов
+     }
+
+     /*!
+      * \brief Парсит pairnode
+      *
+     * Метод парсит connection и сохраняет полученную информацию внутри класса в соответствующих атрибутах
+      */
+
+     void parse_pairnode(boost::property_tree::ptree::value_type const &v)
+     {
+         pairnode pairnoda;
+         pairnoda.address = v.second.get<std::string>("pairnode.<xmlattr>.address");// Устанавливаем  адресс узла, соответстующий иадресу из xml кода
+         pairnoda.port = v.second.get<port_id>("pairnode.<xmlattr>.port");// Устанавливаем  порт узла, соответстующий порту из xml кода
+         pairnoda.config_port = v.second.get<port_id>("pairnode.<xmlattr>.config_port", 100);//Устанавливаем  конфигурацию, соответстующую конфигурации из xml кода
+         pairnodes.push_back(pairnoda);// Пушим узел в вектор узлов
+     }
+
+     /*!
+      * \brief Парсит component
+      *
+     * Метод парсит connection и сохраняет полученную информацию внутри класса в соответствующих атрибутах
+      */
+
+     void parse_component(boost::property_tree::ptree::value_type const &v)
+     {
+         component comp;
+         comp.name = v.second.get<std::string>("<xmlattr>.name");// Задаем имя компонента, соответстующее имени из xml кода
+         comp.kind = v.second.get<std::string>("<xmlattr>.kind");// Задаем тип компонента, соответстующий типу из xml кода
+         components.push_back(comp);// Пушим компонент в вектор компонентов
+     }
+
+     /*!
+      * \brief Парсит connection
+      *
+     * Метод парсит connection и сохраняет полученную информацию внутри класса в соответствующих атрибутах
+      */
+
+     void parse_connection(boost::property_tree::ptree::value_type const &v)
+     {
+         connection conn;
+         conn.source = v.second.get<std::string>("<xmlattr>.source");// Задаем идентификатор компонента у отправляющего компонента
+         conn.source_out = v.second.get<port_id>("<xmlattr>.source_out");// Задаем идентификатор выхода у отправляющего компонента
+         conn.dest = v.second.get<std::string>("<xmlattr>.dest");// Задаем идентификатор компонента, получающего сообщение
+         conn.dest_in = v.second.get<port_id>("<xmlattr>.dest_in");//Задаем идентификатор входа у получающего компонента
+         connections.push_back(conn);//Пушим связь в вектор связей
+     }
+
+     /*!
+      * \brief Парсит map
+      *
+     * Метод парсит map и сохраняет полученную информацию внутри класса в соответствующих атрибутах
+      */
+
+     void parse_map(boost::property_tree::ptree::value_type const &v)
+     {
+         mapping mapp;
+         mapp.comp_name = v.second.get<std::string>("<xmlattr>.instance");// Устанавливаем имя компонента
+         mapp.node_name = v.second.get<std::string>("<xmlattr>.node");// Устанавливаем имя узла
+         mappings.push_back(mapp);// Пушим отображение компонента на сетевой узел в соответствующий вектор
+     }
 };
 
 } // namespace parser
