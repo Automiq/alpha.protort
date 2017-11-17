9e806ecd99c9c27e27e0011e8b1567a9dddac8b0
Aleksey Sampilov <alesha1999@gmail.com>
2017-11-18 03:28:26 +0800

    поставил русскую локализацию и сделал коментарии

--- node/src/logi.h
+++ node/src/logi.h
@@ -8,148 +8,11 @@
 #include <sstream>
 #include <string>
 #include <boost/shared_ptr.hpp>
+#include <clocale>
 
- /*
-  *
-  *    string create_message(string node_name="", string comp_name="", string message, string type){
-        time_t time_logging=time(NULL);
-        stringstream ss_logging;
-        time_t t=time_logging-time_start;
-        ss_logging<<t<<':'<<node_name<<'/'<<comp_name<<':'<<type<<':'<<message;
-        return ss_logging.str();
-    }
-*/
 /*
-
-
-
-class ProtoRt_logger{
-    ProtoRt_logger(){
-        stream=stdout;
-        buf=new char[101];
-        buf[100]='\0';
-        buf_size=100;
-        buf_usesize=0;
-    }
-    ProtoRt_logger(char* file_name){
-        strcat(file_name , ".txt");
-        stream=freopen( file_name , "w" , stdout );
-        buf=new char[101];
-        buf[100]='\0';
-        buf_size=100;
-        buf_usesize=0;
-    }
-    ProtoRt_logger(FILE* file){
-        stream=file;
-        buf=new char[101];
-        buf[100]='\0';
-        buf_size=100;
-        buf_usesize=0;
-    }
-    ~ProtoRt_logger(){
-        this->flush();
-        if(stream!=stdout){
-            fclose(stream);
-        }
-        delete []buf;
-    }
-    void change_file(FILE* file){
-        this->flush();
-        stream=file;
-    }
-    void out_to_console(){
-        this->flush();
-        stream=stdout;
-    }
-    FILE* get_stream(){
-        return stream;
-    }
-    //пишет полученное сообщение в буфер если есть в нём есть место , если нет то выводит буфер в поток
-    bool write(char* message){
-        int message_size=std::strlen(message);
-        if(buf_size){
-
-            if(message_size<=buf_size-buf_usesize){
-
-                memmove(buf , message , message_size);
-                buf_usesize+=message_size;
-
-            }
-            else{
-
-                int first_part_size=buf_size-buf_usesize;
-                memmove(buf+buf_usesize , message , first_part_size);
-                buf_usesize+=first_part_size;
-
-                if(!flush()){
-
-                    //error;
-                    delete[] message;
-                    return false;
-
-                }
-
-                int second_part_size=message_size-first_part_size;
-                memmove(buf+buf_usesize , message , second_part_size);
-                buf_usesize+=second_part_size;
-            }
-
-        }
-        else{
-            if(fwrite(buf , sizeof(char) , buf_usesize , stream)!=message_size||!flush()){
-
-                //error
-                delete[] message;
-                return false;
-            }
-            delete[] message;
-            return true;
-        }
-
-        if(buf_usesize>buf_size*0,9){
-            if(!flush()){
-                //error
-                delete[] message;
-                return false;
-            }
-        }
-        delete[] message;
-        return true;
-    }
-    bool build_message(char* type_message , char* message){
-        time_t ttime=time(NULL);
-        tm* this_time=localtime(&ttime);
-        std::stringstream ss_message;
-        ss_message<<(int)(this_time->tm_mday)<<'/'<<(int)(this_time->tm_mon)<<'/'<<(int)(this_time->tm_year)
-                 <<' '<<(int)(this_time->tm_hour)<<':'
-                 <<(int)(this_time->tm_min)<<':'
-                 <<(int)(this_time->tm_sec)<<' '<<type_message<<'-'<<message<<'\n';
-        char* str=new char[ss_message.str().length()+1];
-        strcpy(str , ss_message.str().c_str());
-        str[ss_message.str().length()]='\0';
-        return write(str);
-    }
-private:
-    //выводит буфер в поток и возвращает смогли он это сделать
-    bool flush(){
-        if(fwrite(buf , sizeof(char) , buf_usesize , stream)!=buf_usesize){
-            return false;
-        }
-        buf_usesize=0;
-        return true;
-    }
-private:
-    FILE *stream; // файл либо stdout
-    char* buf;  // буффер для хранения данных перез записью
-    int buf_size; //размер буффера
-    int buf_usesize; //размер используемой части буффера
-};
-
-
-
+ * Родительский класс, в котором есть метод buil_message создающий сообщения
 */
-
-
 class logger{
 public:
     logger(){}
@@ -168,12 +31,18 @@ protected:
         return  ss_message.str();
     }
 private:
-    friend class logger_component;
-    friend class logger_node;
 };
-
+/*
+ *класс наследуемый от logger , позволяет писать ллоги в файл . Метод "log" использует родительский метод "build_message"
+*/
 class logger_file: public logger{
 public:
+    logger_file(){
+
+        file.open("default_log.txt");
+
+    }
+
     logger_file(std::string &file_name){
         file_name+=".txt";
         file.open(file_name);
@@ -192,6 +61,10 @@ private:
     std::ofstream file;
 };
 
+/*
+ *  класс наследуется от "logger" , имеет метод log который производит запись в stdout и использует родительский метод "build_message"
+*/
+
 class logger_stdout: public logger{
 public:
     logger_stdout(){}
@@ -201,6 +74,12 @@ public:
         std::cout<<message_<<std::endl;
     }
 };
+
+/*
+ *класс наследуемый от logger, в конструкторе инициализирует объект родительского класса,имя и тип компоненты.
+ * указатель на родительский класс указывает в какой объект писать лог.
+ * templ-шаблон для сообщений
+*/
 
 class logger_component : public logger{
 public:
@@ -220,6 +99,13 @@ private:
     std::string templ;
     boost::shared_ptr<logger> log_;
 };
+
+/*
+ *класс наследуемый от logger, в конструкторе инициализирует объект родительского класса,имя узла.
+ * указатель на родительский класс указывает в какой объект писать лог.
+ * templ-шаблон для сообщений
+*/
+
 
 class logger_node : public logger{
 public:
--- node/src/main.cpp
+++ node/src/main.cpp
@@ -1,9 +1,11 @@
 #include "node_settings.h"
 #include "node.h"
+#include <clocale>
 
 using namespace alpha::protort;
 int main(int argc, const char *argv[])
 {
+    setlocale(LC_ALL, "rus");
     node::node_settings settings;
     if (!settings.parse(argc,argv))
         return 1;
